5a47e85e09423ec6c9f8a5e453f1777394fa6628
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-03-05 19:09:15 -0600

    form is fully functional and some styling!

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 'use strict';
-console.log('ready to rock');
 
 /////////////////////Constructor////////////////////////
 function Store(storeName, minCustomers, maxCustomers, unitsPerCustomer) {
@@ -11,16 +10,8 @@ function Store(storeName, minCustomers, maxCustomers, unitsPerCustomer) {
   this.cookiesPerHour = [];
 }
 
-//////////////////////New Stores/////////////////////////////
-var Seattle = new Store('Seattle', 23, 65, 6.3);
-var Tokyo = new Store('Tokyo', 3, 24, 1.2);
-var Dubai = new Store('Dubai', 11, 38, 3.7);
-var Paris = new Store('Paris', 20, 38, 2.3);
-var Lima = new Store('Lima', 2, 16, 4.6);
-
 /////////////////////Global Variables/////////////////////////
 var hours = ['6:00am','7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
-var stores = [Seattle, Tokyo, Dubai, Paris, Lima];
 
 ////////////////////Prototype Functions////////////////////////
 Store.prototype.getCustomers = function() {
@@ -35,57 +26,38 @@ Store.prototype.getCookies = function() {
   }
 };
 
-//////////////////////////Function Calls/////////////////////////
-Seattle.getCustomers();
-Seattle.getCookies();
-console.log(Seattle.customersPerHour);
-console.log(Seattle.cookiesPerHour);
-console.log(Seattle.totalCookies);
-Tokyo.getCustomers();
-Tokyo.getCookies();
-console.log(Tokyo.customersPerHour);
-console.log(Tokyo.cookiesPerHour);
-Dubai.getCustomers();
-Dubai.getCookies();
-console.log(Dubai.customersPerHour);
-console.log(Dubai.cookiesPerHour);
-Paris.getCustomers();
-Paris.getCookies();
-console.log(Paris.customersPerHour);
-console.log(Paris.cookiesPerHour);
-Lima.getCustomers();
-Lima.getCookies();
-console.log(Lima.customersPerHour);
-console.log(Lima.cookiesPerHour);
-
 ///////////////////////Render////////////////////////////////
 var salesTable = document.getElementById('sales-table');
-var headRow = document.createElement('tr');
-var headRowBlank = document.createElement('th');
-headRowBlank.setAttribute('scope', 'row');
-headRowBlank.textContent = '';
-headRow.appendChild(headRowBlank);
 
-for ( let i = 0; i < hours.length; i++ ) {
-  var hourHead = document.createElement('th');
-  hourHead.setAttribute('scope', 'col');
-  hourHead.textContent = hours[i];
-  headRow.appendChild(hourHead);
-  salesTable.appendChild(headRow);
+function renderHeader(){
+  var headRow = document.createElement('tr');
+  var headRowBlank = document.createElement('th');
+  headRowBlank.textContent = '';
+  headRow.appendChild(headRowBlank);
+
+  for ( let i = 0; i < hours.length; i++ ) {
+    var hourHead = document.createElement('th');
+    hourHead.textContent = hours[i];
+    headRow.appendChild(hourHead);
+    salesTable.appendChild(headRow);
+  }
+  
+  var totalHead = document.createElement('th');
+  totalHead.textContent = 'Daily Location Totals';
+  headRow.appendChild(totalHead);
 }
 
-var totalHead = document.createElement('th');
-totalHead.setAttribute('scope', 'col');
-totalHead.textContent = 'Daily Location Totals';
-headRow.appendChild(totalHead);
+renderHeader();
 
 Store.prototype.render = function() {
   var oldTotal = document.getElementById('removeTotal');
   if (oldTotal) {
     oldTotal.parentNode.removeChild(oldTotal);
   }
+
   var storeRow = document.createElement('tr');
   var storeCell = document.createElement('td');
+
   storeCell.textContent = this.storeName;
   storeRow.appendChild(storeCell);
   for ( let i = 0; i < hours.length; i++ ) {
@@ -101,11 +73,30 @@ Store.prototype.render = function() {
 
   this.cookiesPerHour.push(total);
   totalCell.textContent = total;
-
   storeRow.appendChild(totalCell);
   salesTable.appendChild(storeRow);
-
 };
+
+//////////////////////New Stores/////////////////////////////
+var Seattle = new Store('Seattle', 23, 65, 6.3);
+var Tokyo = new Store('Tokyo', 3, 24, 1.2);
+var Dubai = new Store('Dubai', 11, 38, 3.7);
+var Paris = new Store('Paris', 20, 38, 2.3);
+var Lima = new Store('Lima', 2, 16, 4.6);
+
+//////////////////////////Function Calls/////////////////////////
+Seattle.getCustomers();
+Seattle.getCookies();
+Tokyo.getCustomers();
+Tokyo.getCookies();
+Dubai.getCustomers();
+Dubai.getCookies();
+Paris.getCustomers();
+Paris.getCookies();
+Lima.getCustomers();
+Lima.getCookies();
+
+var stores = [Seattle, Tokyo, Dubai, Paris, Lima];
 
 for( let i = 0; i < stores.length; i++ ) {
   stores[i].render();
@@ -118,7 +109,6 @@ function totalFoot() {
   totalFoot.setAttribute('scope', 'row');
   totalFoot.textContent = 'Totals';
   footRow.appendChild(totalFoot);
-
   for ( let i = 0; i < hours.length + 1; i++ ) {
     var storeFoot = document.createElement('td');
     var hourlyTotal = 0;
@@ -132,17 +122,15 @@ function totalFoot() {
 }
 
 totalFoot();
-////////////////////Photobook////////////////////////
+
+//////////////////Photobook////////////////////////
 function photobook(imgs) {
   var expandImg =
   document.getElementById('expandedImg');
   var imgText =
   document.getElementById('imgtext');
-
   expandImg.src = imgs.src;
-
   imgText.innerHTML = imgs.alt;
-
   expandImg.parentElement.style.display
   = 'block';
 }
@@ -150,7 +138,6 @@ function photobook(imgs) {
 ///////////////////////Form/////////////////////////
 function handleFormSubmission(event){
   event.preventDefault();
-
   var storeInput = document.getElementById('storeName');
   var storeValue = storeInput.value;
   var minInput = document.getElementById('minCustomers');
@@ -159,15 +146,14 @@ function handleFormSubmission(event){
   var maxValue = maxInput.value;
   var avgCookieInput = document.getElementById('unitsPerCustomer');
   var avgCookieValue = avgCookieInput.value;
-
   var newStore = new Store(storeValue, minValue, maxValue, avgCookieValue);
 
   newStore.getCustomers();
   newStore.getCookies();
+  stores.push(newStore);
   newStore.render();
-  newStore.totalFoot();
+  totalFoot();
 }
 
 var formElement = document.getElementById('new-store');
-
 formElement.addEventListener('submit', handleFormSubmission);
